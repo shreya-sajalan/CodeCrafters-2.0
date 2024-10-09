ca100c51e5fe8c427c77c0c61ab470ff35530fdf
shreya-sajalan <shreyasajalan@gmail.com>
2024-10-09 10:50:32 +0000

    day 3

---
+++ main.py
@@ -0,0 +1,23 @@
+#print("hello world")
+#print("shreya sajalan")
+#name=input("enter ur name")
+#print("Welcome",name)
+#string="laptop
+#boolean=True
+#integer=12
+#flt=50.1
+''' x=int(input("enter a number"))
+y=int(input("enter another number"))
+if x==y:
+    print(x,"is equal to",y)
+else:
+    print("they are not equal")'''
+k=10
+l=80
+s=k+l
+if s>50:
+    print("greater")
+elif s<50:
+    print("less than")
+else:
+    print("equal")
